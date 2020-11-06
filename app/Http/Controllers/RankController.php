2b4abbbe0eff5f41741d6a5d3338e7340f0c2008
Jetaime <oncwnuIW-bXt3K9btDrDHPzhbdAk@git.weixin.qq.com>
2020-11-06 15:07:05 +0800

    2020年11月6日 15:07:04

--- app/Http/Controllers/RankController.php
+++ app/Http/Controllers/RankController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Rank;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Maatwebsite\Excel\Concerns\FromCollection;
+
+
 class RankController extends Controller
 {
-    
+
     public function login($code){
-        return $code;
+        $appid=config('wechat.miniprogram.appid');
+        $secret=config('wechat.miniprogram.secret');
+       $res= Http::get("https://api.weixin.qq.com/sns/jscode2session?appid='.$appid.'&secret='.$secret.'&js_code='.$code.'&grant_type=authorization_code",[
+            'code'=>$code
+        ]);
+        return $res;
     }
     /**
      * Display a listing of the resource.
