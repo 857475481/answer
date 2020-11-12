716ba1268aeb901b68bc6f31c3dd1f7bdebaa3d6
root <root@localhost.localdomain>
2020-11-12 20:11:19 +0800

    2020年11月12

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'qa'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'SWP4syAFJfcraG4Y'),
+            'password' => env('DB_PASSWORD', 'NGLLFnjZehwJfjjZ'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
